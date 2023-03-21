5a58f1445d8d6166d81141ba8e65679af5c75ab1
qiliangfan <1035847702@qq.com>
2023-03-21 18:20:47 +0800

    update submodule

--- doc_addons/expose_svc.sh
+++ doc_addons/expose_svc.sh
@@ -10,3 +10,6 @@ kubectl port-forward svc/jaeger-tracing-query -n observe 8081:80 --address 0.0.0
 # The Grafana
 kubectl port-forward svc/grafana -n observe 8080:80 --address 0.0.0.0
 # kubectl get secret --namespace observe grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+
+# The Prometheus （注意：内部通过代理真实的转向是9090而非80）
+kubectl port-forward svc/prometheus-server -n observe --address 0.0.0.0 9090:80
